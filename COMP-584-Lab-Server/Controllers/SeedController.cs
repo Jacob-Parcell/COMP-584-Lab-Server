515d4a1cf75bae2076f0ebf100cbab725427e403
Jacob-Parcell <jakeparcell14@gmail.com>
2025-10-03 17:48:52 -0700

    Added records and Seeds

---
+++ COMP-584-Lab-Server/Controllers/SeedController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldModel;
+
+namespace COMP_584_Lab_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(Comp584labContext context) : ControllerBase
+    {
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("Countries")]
+        public async Task<ActionResult> PostCountries()
+        {
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        // POST: api/Cities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("Cities")]
+        public async Task<ActionResult> PostCities()
+        {
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+
+}
